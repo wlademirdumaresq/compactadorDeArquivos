7affbf4b486a52aaaa60b01f904d95e62d36d8e3
Wlademir Dumaresq <wlademir.dumaresq@gmail.com>
2019-10-11 00:46:10 -0300

    finalizado

--- projeto/src/br/edu/imd/edb/application/Main.java
+++ projeto/src/br/edu/imd/edb/application/Main.java
@@ -3,10 +3,7 @@ package br.edu.imd.edb.application;
 import br.edu.imd.edb.heap.Heap;
 import br.edu.imd.edb.tree.Node;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 import static br.edu.imd.edb.util.TBinary.LBinary;
@@ -15,6 +12,8 @@ import static br.edu.imd.edb.util.TBinary.LBinary;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
+        //  if (args[0].equals("compress")) {
         Map<Integer, Integer> map = new HashMap<>();
 
         try {
@@ -33,12 +32,12 @@ public class Main {
                         }
                     }
 
-                    if (map.containsKey(257) && leitor.hasNextLine()) {
-                        int value = (map.get(257)) + 1;
-                        map.remove(257);
-                        map.put(257, value);
-                    } else if (!(map.containsKey(257)) && leitor.hasNextLine()) {
-                        map.put(257, 1);
+                    if (map.containsKey(300) && leitor.hasNextLine()) {
+                        int value = (map.get(300)) + 1;
+                        map.remove(300);
+                        map.put(300, value);
+                    } else if (!(map.containsKey(300)) && leitor.hasNextLine()) {
+                        map.put(300, 1);
                     }
                 }
                 map.put(258, 1);
@@ -80,44 +79,158 @@ public class Main {
         //System.out.println( tree.printTree(0));
         Map<Character, String> binari = new HashMap<>();
 
-        FileWriter tabelaCod = new FileWriter("filename.edt");
+        FileWriter tabelaCod = new FileWriter("filename.edt"); //"filename.edt"
 
         for (int i = 0; i < bit.length; ) {
-
             tabelaCod.write((char) Integer.parseInt(bit[i]) + "=" + bit[i + 1] + "\n");
             binari.put((char) Integer.parseInt(bit[i]), bit[i + 1]);
             i += 2;
         }
+        //tabelaCod.write(String.valueOf(binari));
         tabelaCod.close();
 
-        FileWriter b = new FileWriter("filename.edz");
-        FileReader arq = new FileReader(args[0]);
+        FileOutputStream b = new FileOutputStream("filename.edz"); //"filename.edz"
+        FileReader arq = new FileReader("teste.txt");
 
         Scanner leitor = new Scanner(arq);
         int contador = 0;
+        BitSet bitSet = new BitSet();
+        String bits = "";
         while (leitor.hasNextLine()) {
             String linha = leitor.nextLine();
             for (int i = 0; i < linha.length(); i++) {
                 if (binari.containsKey(linha.charAt(i))) {
-                    b.write(binari.get(linha.charAt(i)));
-                    for (int j = 0; j < binari.get(linha.charAt(i)).length();j++){
-                        contador +=1;
+                    for (int j = 0; j < binari.get(linha.charAt(i)).length(); j++) {
+                        if (binari.get(linha.charAt(i)).charAt(j) == '1') {
+                            bits += "1";
+                            bitSet.set(contador);
+                        } else {
+                            bits += "0";
+                            bitSet.set(contador, false);
+                        }
+                        contador += 1;
                     }
                 }
             }
-        }
-        b.write(binari.get((char) 258));
-        contador +=binari.get((char) 258).length();
-
-        if(contador%8 == 0){
-            b.close();
-        }else{
-            int multiplicacao = (int) ((1-(((float)contador/8) - (contador/8)))*8);
-            for(int i = 0; i<multiplicacao; i++){
-                b.write('0');
+            if(leitor.hasNextLine()){
+                for (int j = 0; j < binari.get((char)300).length(); j++) {
+                    if (binari.get((char)300).charAt(j) == '1') {
+                        bits += "1";
+                        bitSet.set(contador);
+                    } else {
+                        bits += "0";
+                        bitSet.set(contador, false);
+                    }
+                    contador += 1;
+                }
             }
-            b.close();
         }
 
+
+        for (int j = 0; j < binari.get((char) 258).length(); j++) {
+            if (binari.get((char) 258).charAt(j) == '1') {
+                bits += "1";
+                bitSet.set(contador);
+            } else {
+                bits += "0";
+                bitSet.set(contador, false);
+            }
+            contador += 1;
+        }
+        int multiplicacao=0;
+        if (contador % 8 == 0) {
+
+            b.write(bitSet.toByteArray());
+            b.close();
+            arq.close();
+        } else {
+             multiplicacao = (int) ((1 - (((float) contador / 8) - (contador / 8))) * 8);
+            for (int i = 0; i < multiplicacao; i++) {
+                bitSet.set(contador);
+                contador += 1;
+                bits += "1";
+            }
+            b.write(bitSet.toByteArray());
+            b.close();
+            arq.close();
+        }
+
+        FileReader mensagem = new FileReader("filename.edz");
+        FileReader dicionario = new FileReader("filename.edt");
+
+        Map<String, Character> letra = new HashMap<>();
+        Map<Character, String> codigo = new HashMap<>();
+        Scanner ler = new Scanner(dicionario);
+        String[] l;
+        while (ler.hasNextLine()) {
+            l = ler.nextLine().split("=");
+            for (int i = 0; i < l.length; ) {
+                letra.put(l[i + 1], l[i].charAt(0));
+                codigo.put(l[i].charAt(0),l[i + 1]);
+                i += 2;
+            }
+
+        }
+        dicionario.close();
+
+
+        FileWriter escreve = new FileWriter("Teste2.txt");
+        FileInputStream fs = new FileInputStream("filename.edz");
+
+        ler = new Scanner(mensagem);
+        String l1;
+        byte[] bytes = fs.readAllBytes();
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 0; i < bytes.length; i++) {
+            str.append(new StringBuilder(String.format("%8s", Integer.toBinaryString(bytes[i] & 0xFF)).replace(" ", "0")).reverse());
+        }
+        String comparador = "";
+        FileWriter novo = new FileWriter("novo.txt");
+
+        int cont =0;
+        for (int j = 0; j < str.length(); j++) {
+            comparador+=str.charAt(j);
+            if (comparador.equals(codigo.get((char) 258))) {
+                novo.close();
+
+            } else if (codigo.containsValue(comparador)) {
+                cont+= comparador.length();
+                if(comparador.equals(codigo.get((char) 300))){
+                    novo.write("\n");
+                    comparador="";
+
+                }else{
+                    novo.write(letra.get(comparador));
+                    comparador="";
+                }
+
+
+            }
+        }
+        str.trimToSize();
+
+        // } else if (args[0].equals("Extract")) {
+//            try{
+//
+//                File mensagem = new File(args[1]);
+//                File dicionario = new File(args[2]);
+//                if(mensagem.exists()){
+//                    if(dicionario.exists()){
+//
+//
+//
+//                    }
+//                }
+//
+//            }catch (IOException){
+//                System.out.println("Parâmemtro inválido!");
+//            }
+//
+//        }else {
+//            System.out.println("Parâmemtro inválido!");
+//        }
+
+        //      }
     }
 }
